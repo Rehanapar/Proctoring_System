3c4b48dbd24a5d9db713045896f11fb60bf298ba
Rehanapar <132186859+Rehanapar@users.noreply.github.com>
2024-11-15 13:09:21 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+    SECRET_KEY = 'xyz123'
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = 'root@123'
+    MYSQL_DB = 'Registration'
+    SECRET_KEY = 'gyfyd456871242354sdfsrytere'
